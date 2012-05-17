310d990243cb0512f319a2b6ce6d4febea4075ec
Mark Markarian <mark@interzonedev.com>
2012-05-17 01:16:22 -0700

    Added lots of helper methods to FunctionalTestHelper.
    Started implementing TestViewAllUsersFunctionalTest.testViewAllUsersWithUsers.

--- src/test/java/com/interzonedev/herokuspringdemo/functionaltest/FunctionalTestHelper.java
+++ src/test/java/com/interzonedev/herokuspringdemo/functionaltest/FunctionalTestHelper.java
@@ -5,8 +5,13 @@ import java.util.Properties;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 @Named("functionalTestHelper")
 public class FunctionalTestHelper {
@@ -48,4 +53,48 @@ public class FunctionalTestHelper {
 		driver.get(functionalTestProperties.getProperty("applicationUrl") + url);
 	}
 
+	public Long getDefaultElementWaitTimeoutInSeconds() {
+		return Long.parseLong(functionalTestProperties.getProperty("elementWaitTimeoutInSeconds"));
+	}
+
+	public void waitForElement(WebDriver driver, By by) {
+		waitForElement(driver, by, getDefaultElementWaitTimeoutInSeconds());
+	}
+
+	public void waitForElement(WebDriver driver, By by, long timeOutInSeconds) {
+		waitForElementWithText(driver, by, null, timeOutInSeconds);
+	}
+
+	public void waitForElementWithText(WebDriver driver, By by, String text) {
+		waitForElementWithText(driver, by, text, getDefaultElementWaitTimeoutInSeconds());
+	}
+
+	public void waitForElementWithText(WebDriver driver, final By by, final String text, long timeOutInSeconds) {
+		ExpectedCondition<Boolean> pageLoadCondition = new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver d) {
+				WebElement element = d.findElement(by);
+				return ((null == text) || element.getText().equals(text));
+			}
+		};
+		Wait<WebDriver> pageLoadWait = new WebDriverWait(driver, timeOutInSeconds);
+
+		pageLoadWait.until(pageLoadCondition);
+	}
+
+	public WebElement waitForAndGetElement(WebDriver driver, By by) {
+		return waitForAndGetElementWithText(driver, by, null, getDefaultElementWaitTimeoutInSeconds());
+	}
+
+	public WebElement waitForAndGetElement(WebDriver driver, By by, long timeOutInSeconds) {
+		return waitForAndGetElementWithText(driver, by, null, timeOutInSeconds);
+	}
+
+	public WebElement waitForAndGetElementWithText(WebDriver driver, By by, String text) {
+		return waitForAndGetElementWithText(driver, by, text, getDefaultElementWaitTimeoutInSeconds());
+	}
+
+	public WebElement waitForAndGetElementWithText(WebDriver driver, By by, String text, long timeOutInSeconds) {
+		waitForElementWithText(driver, by, text, timeOutInSeconds);
+		return driver.findElement(by);
+	}
 }
--- src/test/java/com/interzonedev/herokuspringdemo/users/TestViewAllUsersFunctionalTest.java
+++ src/test/java/com/interzonedev/herokuspringdemo/users/TestViewAllUsersFunctionalTest.java
@@ -1,6 +1,9 @@
 package com.interzonedev.herokuspringdemo.users;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import com.interzonedev.herokuspringdemo.HerokuSpringDemoAbstractFunctionalTest;
 import com.interzonedev.sprintfix.dataset.DataSet;
@@ -11,11 +14,23 @@ public class TestViewAllUsersFunctionalTest extends HerokuSpringDemoAbstractFunc
 	@DataSet(filename = "dataset/users/multiUsersDataSet.xml", dataSourceBeanId = "dataSource")
 	public void testViewAllUsersWithUsers() {
 		log.debug("testViewAllUsersWithUsers");
+
+		functionalTestHelper.openPage(driver, "/users");
+
+		WebElement contentContainer = functionalTestHelper.waitForAndGetElement(driver, By.id("contentContainer"));
+
+		WebElement pageHeader = driver.findElement(By.cssSelector("div#contentContainer div.pageHeader"));
+
+		Assert.assertNotNull(contentContainer);
+		Assert.assertNotNull(pageHeader);
+		Assert.assertTrue(pageHeader.getText().endsWith("View All Users"));
 	}
 
 	@Test
 	@DataSet(filename = "dataset/users/emptyUsersDataSet.xml", dataSourceBeanId = "dataSource")
 	public void testViewAllUsersNoUsers() {
 		log.debug("testViewAllUsersNoUsers");
+
+		Assert.assertTrue(true);
 	}
 }
